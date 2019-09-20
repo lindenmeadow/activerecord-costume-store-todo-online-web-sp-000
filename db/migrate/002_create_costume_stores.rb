a3708f9d95a5f80c7a4e00a331dbf80c6a64c042
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-09-20 23:08:59 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,7 +6,9 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :location
       t.integer :costume_inventory
       t.integer :num_of_employees
-
+      t.boolean :still_in_business
+      t. :opening_time
+      t. :closing_time
     end
   end
 end
