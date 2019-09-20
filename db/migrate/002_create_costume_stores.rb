dba1a4eb8b93caea50020f607134d8bfbddca1c5
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-09-20 23:06:56 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,7 +4,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
     create_table costume_stores do |t|
       t.string :name
       t.string :location
-      t.integer :inventory
+      t.integer :costume_inventory
       t.integer :employees
 
     end
